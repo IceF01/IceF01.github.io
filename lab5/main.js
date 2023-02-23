f3601ad94b60ed04da1e03287722f796c23e5bc8
IceF01 <aigerimturegeldyyeva@nu.edu.kz>
2023-02-23 14:44:12 +0600

    lab5 finished!

---
+++ lab5/main.js
@@ -0,0 +1,117 @@
+
+let column_1 = [3, 5, 7, 6, 8, 3, 6, 2, 5, 2, 7, 4, 2, 6, 7, 4, 2, 4, 6, 3, 5, 8, 7, 2, 1, 3, 5, 7, 3, 2]
+let column_2 = [4, 5, 2, 6, 7, 3, 4, 7, 8, 4, 2, 1, 4, 6, 7, 3, 2, 5, 7, 2, 1, 5, 7, 8, 4, 2, 1, 1, 5, 6]
+let column_3 = [45, 34, 32, 32, 43, 54, 34, 22, 32, 43, 54, 65, 45, 32, 13, 24, 35, 46, 54, 53, 42, 24, 35, 46, 57, 68, 76, 75, 64, 53]
+let column_4 = [42, 32, 43, 54, 65, 72, 43, 54, 65, 45, 33, 24, 35, 43, 22, 32, 43, 54, 65, 45, 92, 32, 43, 54, 34, 22, 32, 43, 54, 65]
+
+
+
+
+let sum_1 = column_1[0] + column_2[0] + column_3[0] + column_4[0]
+let sum_2 = column_1[1] + column_2[1] + column_3[1] + column_4[1]
+let sum_3 = column_1[2] + column_2[2] + column_3[2] + column_4[2]
+let sum_4 = column_1[3] + column_2[3] + column_3[3] + column_4[3]
+let sum_5 = column_1[4] + column_2[4] + column_3[4] + column_4[4]
+let sum_6 = column_1[5] + column_2[5] + column_3[5] + column_4[5]
+let sum_7 = column_1[6] + column_2[6] + column_3[6] + column_4[6]
+let sum_8 = column_1[7] + column_2[7] + column_3[7] + column_4[7]
+let sum_9 = column_1[8] + column_2[8] + column_3[8] + column_4[8]
+let sum_10 = column_1[9] + column_2[9] + column_3[9] + column_4[9]
+let sum_11 = column_1[10] + column_2[10] + column_3[10] + column_4[10]
+let sum_12 = column_1[11] + column_2[11] + column_3[11] + column_4[11]
+let sum_13 = column_1[12] + column_2[12] + column_3[12] + column_4[12]
+let sum_14 = column_1[13] + column_2[13] + column_3[13] + column_4[13]
+let sum_15 = column_1[14] + column_2[14] + column_3[14] + column_4[14]
+let sum_16 = column_1[15] + column_2[15] + column_3[15] + column_4[15]
+let sum_17 = column_1[16] + column_2[16] + column_3[16] + column_4[16]
+let sum_18 = column_1[17] + column_2[17] + column_3[17] + column_4[17]
+let sum_19 = column_1[18] + column_2[18] + column_3[18] + column_4[18]
+let sum_20 = column_1[19] + column_2[19] + column_3[19] + column_4[19]
+let sum_21 = column_1[20] + column_2[20] + column_3[20] + column_4[20]
+let sum_22 = column_1[21] + column_2[21] + column_3[21] + column_4[21]
+let sum_23 = column_1[22] + column_2[22] + column_3[22] + column_4[22]
+let sum_24 = column_1[23] + column_2[23] + column_3[23] + column_4[23]
+let sum_25 = column_1[24] + column_2[24] + column_3[24] + column_4[24]
+let sum_26 = column_1[25] + column_2[25] + column_3[25] + column_4[25]
+let sum_27 = column_1[26] + column_2[26] + column_3[26] + column_4[26]
+let sum_28 = column_1[27] + column_2[27] + column_3[27] + column_4[27]
+let sum_29 = column_1[28] + column_2[28] + column_3[28] + column_4[28]
+let sum_30 = column_1[29] + column_2[29] + column_3[29] + column_4[29]
+
+
+let sums = [
+    sum_1,
+    sum_2,
+    sum_3,
+    sum_4,
+    sum_5,
+    sum_6,
+    sum_7,
+    sum_8,
+    sum_9,
+    sum_10,
+    sum_11,
+    sum_12,
+    sum_13,
+    sum_14,
+    sum_15,
+    sum_16,
+    sum_17,
+    sum_18,
+    sum_19,
+    sum_20,
+    sum_21,
+    sum_22,
+    sum_23,
+    sum_24,
+    sum_25,
+    sum_26,
+    sum_27,
+    sum_28,
+    sum_29,
+    sum_30,
+
+]
+
+let total_score = sum_1 +
+    sum_2 +
+    sum_3 +
+    sum_4 +
+    sum_5 +
+    sum_6 +
+    sum_7 +
+    sum_8 +
+    sum_9 +
+    sum_10 +
+    sum_11 +
+    sum_12 +
+    sum_13 +
+    sum_14 +
+    sum_15 +
+    sum_16 +
+    sum_17 +
+    sum_18 +
+    sum_19 +
+    sum_20 +
+    sum_21 +
+    sum_22 +
+    sum_23 +
+    sum_24 +
+    sum_25 +
+    sum_26 +
+    sum_27 +
+    sum_28 +
+    sum_29 +
+    sum_30
+
+
+const results = {
+    col1: column_1,
+    col2: column_2,
+    col3: column_3,
+    col4: column_4,
+    sums: sums,
+    total_score:total_score
+
+}
+console.log(results)
